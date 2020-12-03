f4f1e3669950f20e9b957d9f9e09641a8afcf6c0
Hannah Rose <hannaesthetic.r@gmail.com>
2020-12-03 08:57:50 +0000

    Step one solution

--- Assets/Days/Day3/Scripts/SolutionDayThree.cs
+++ Assets/Days/Day3/Scripts/SolutionDayThree.cs
@@ -6,6 +6,25 @@ namespace AdventOfCode.DayThree
 {
   public class SolutionDayThree : MonoBehaviour
   {
+    public const int MovementX = 3;
+    public const int MovementY = 1;
+    public ToboganMap Map;
 
+    [ContextMenu("Solve Step One")]
+    public void SolveStepOne()
+    {
+      int trees = 0;
+      Vector2Int position = Vector2Int.zero;
+      do
+      {
+        if (Map.HitTree(position.x, position.y))
+        {
+          trees++;
+        }
+        position.x = (position.x + MovementX) % Map.Size.x;
+        position.y += MovementY;
+      } while (position.y < Map.Size.y);
+      Debug.Log($"Solution complete, hit {trees} trees");
+    }
   }
 }
--- Assets/Days/Day3/Scripts/ToboganMap.cs
+++ Assets/Days/Day3/Scripts/ToboganMap.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 namespace AdventOfCode.DayThree
 {
@@ -9,6 +10,7 @@ namespace AdventOfCode.DayThree
   {
     [TextAreaAttribute(3, 10)] public string UnparsedData;
     public MapLine[] ParsedData;
+    public Vector2Int Size;
 
     [ContextMenu("Parse data")]
     public void Parse()
@@ -16,11 +18,24 @@ namespace AdventOfCode.DayThree
       string[] split = UnparsedData.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
       ParsedData = new MapLine[split.Length];
 
+      // just gonna assume all lines the same length yay
+      Size = new Vector2Int(split[0].Length, split.Length);
+
       for (int i = 0; i < split.Length; i++)
       {
-        ParsedData[i] = new MapLine(split[i]);
+        ParsedData[i] = new MapLine(split[i], Size.x);
       }
       Debug.Log("Map parsed");
+    }
+
+    public bool HitTree(int x, int y)
+    {
+      if (x < 0 || x >= Size.x || y < 0 || y >= Size.y)
+      {
+        Debug.LogError($"Tree check made out of map bounds: ({x}, {y})");
+        return false;
+      }
+      return ParsedData[y].LineData[x];
     }
   }
 
@@ -30,10 +45,15 @@ namespace AdventOfCode.DayThree
     public const char TreeChar = '#';
     public bool[] LineData;
     
-    public MapLine(string data)
+    public MapLine(string data, int size)
     {
-      LineData = new bool[data.Length];
-      for (int i = 0; i < data.Length; i++)
+      LineData = new bool[size];
+      if (size > data.Length)
+      {
+        Debug.LogError("Map line too short");
+        return;
+      }
+      for (int i = 0; i < size; i++)
       {
         LineData[i] = data[i] == TreeChar;
       }
