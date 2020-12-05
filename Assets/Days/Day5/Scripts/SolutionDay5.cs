e630096a87a86d4f85baeb189a699005deefde01
Hannah Rose <hannaesthetic.r@gmail.com>
2020-12-05 09:01:00 +0000

    Day 5, process 1

---
+++ Assets/Days/Day5/Scripts/SolutionDay5.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AdventOfCode.DayFive
+{
+  public class SolutionDay5 : MonoBehaviour
+  {
+    public const char FRONT_CHAR = 'F';
+    public const char BACK_CHAR = 'B';
+    public const char RIGHT_CHAR = 'R';
+    public const char LEFT_CHAR = 'L';
+    public const int ROW_COUNT = 7;
+    public const int COLUMN_COUNT = 3;
+
+    public string input;
+
+    [ContextMenu("Find Seat")]
+    private void CheckInput()
+    {
+      Vector2Int seatPosition = FindSeat(input);
+      Debug.Log($"Row: {seatPosition.x}, column: {seatPosition.y}, ID: {seatPosition.x * Mathf.Pow(2f, COLUMN_COUNT) + seatPosition.y}");
+    }
+
+    private Vector2Int FindSeat(string input)
+    {
+      string rows = input.Substring(0, ROW_COUNT);
+      string columns = input.Substring(ROW_COUNT, COLUMN_COUNT);
+      
+      rows = rows.Replace(BACK_CHAR, '1').Replace(FRONT_CHAR, '0');
+      columns = columns.Replace(LEFT_CHAR, '0').Replace(RIGHT_CHAR, '1');
+
+      int row = Convert.ToInt32(rows, 2);
+      int column = Convert.ToInt32(columns, 2);
+
+      return new Vector2Int(row, column);
+    }
+  }
+}
